7dca6cbd1a38a3a370aad311e5d2870bc95a862b
SHA <33160317+shaneymar@users.noreply.github.com>
2023-04-30 22:14:24 +0530

    updated css

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import { useState } from 'react';
 import './App.css';
 import UserTable from './tables/UserTable';
 import AddUserForm from './forms/AddUserForm';
+import EditUserForm from './forms/EditUserForm';
 
 function App() {
 
   const usersData = [
-    // {id:1, name: "Shankar", username:"@sha28", email:"shankar3156@outlook.com"},
-    // {id:1, name: "Gopal", username:"@gopalkrishna31", email:"gopalKrish56@outlook.com"},
-    // {id:1, name: "Ram", username:"@ram@46", email:"rampgoblan@outlook.com"},
+    {id:1, name: "Shankar", username:"@sha28", email:"shankar3156@outlook.com"},
+    
   ]
 
   //Adding new User to Table
@@ -23,16 +23,15 @@ function App() {
   }
 
   //Edit User/Update User
-  const editData = (user) =>{
+  const editData = (user)=>{
     setEditing(true);
-    setCurrentUser({id:user.id, name:user.name, username:user.username, email:user.email});
+    setCurrentUser({id:user.id,name:user.name,username:user.username, email:user.email});
+}
 
-  }
-
-  const updateUser = (id, updatedUser) => {
-    setEditing(false);
-    setUsers(users.map((user) => (user.id === !id?updatedUser : user)))
-  }
+   const updateUser = (id,updatedUser)=>{
+        setEditing(false);
+        setUsers(users.map((user)=>(user.id===id?updatedUser:user)))
+    }
   //Read setting state
   const [users, setUsers] = useState(usersData);
 
@@ -48,13 +47,33 @@ function App() {
       <h1>CRUD Application</h1>
       <div className='flex-row'>
         <div className='flex-large'>
-            <h2>Add User</h2>
-            <AddUserForm addUser={addUser}/>
+            {
+              editing ? (
+                <div>
+                  <h2>Edit User</h2>
+                  <EditUserForm 
+                    setEditing = {setEditing}
+                    currentUser = {currentUser}
+                    updateUser = {updateUser}
+                  />
+                </div> 
+              ) : (
+                <div>
+                  <h2>Add user</h2>
+                   <AddUserForm addUser={addUser}/>
+                </div>
+              )
+            }
+           
         </div>
         <hr />
         <div className='flex-large'>
           <h2>View Users</h2>
-          <UserTable users={users}  deleteUser={deleteUser} editData={editData}/>
+          <UserTable 
+          users={users}  
+          deleteUser={deleteUser} 
+          editData={editData} 
+          />
         </div>
       </div>
     </div>
--- src/forms/EditUserForm.js
+++ src/forms/EditUserForm.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const EditUserForm = () => {
+const EditUserForm = (props) => {
 
+const [user, setUser] = useState(props.currentUser);
 
-    
-
+const handleInputChange = (e) =>{
+    const {name, value} = e.target
+    setUser({...user, [name]:value});
+}
   return (
-    <div>EditUserForm</div>
+    <form onSubmit={
+        e => {
+            e.preventDefault();
+            if(!user.name || !user.username || !user.email ) return;
+            props.updateUser(user.id, user);
+            
+        }
+    }>
+        <label htmlFor='name'>Name</label>
+        <input type="text"  name="name" value={user.name} id="name" onChange={handleInputChange} />
+
+        <label htmlFor='username'>Username</label>
+        <input type="text" name="username" value={user.username} id="username" onChange={handleInputChange}/>
+
+        <label htmlFor='email'>Email</label>
+        <input type="email" name="email" value={user.email} id="email" onChange={handleInputChange}/>
+        <button>Update user</button>
+        <button onClick={() => props.setEditing(false)} className='button muted-button'>Cancel</button>
+      </form>
   )
 }
 
